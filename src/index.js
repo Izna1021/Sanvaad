9a6b5cc525ab1dd4caf817fa779f656c4642a573
Bibi Izna <izna8402@gmail.com>
2023-10-03 02:56:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports'; // Import the AWS Amplify configuration
+
+Amplify.configure(awsconfig); // Configure AWS Amplify
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to measure performance, uncomment the line below.
+// reportWebVitals();
 
