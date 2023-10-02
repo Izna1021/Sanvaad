872c18b793693b1c14c6f8e118554485c16667b7
Bibi Izna <izna8402@gmail.com>
2023-10-03 02:52:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import './App.css';
+import FileUploader from './FileUploader';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>File Upload to AWS S3</h1>
+      </header>
+      <main>
+        <FileUploader />
+      </main>
+    </div>
+  );
+}
+
+export default App;
 
